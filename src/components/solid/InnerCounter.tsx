0ce31c6794b768443b629d8d5edd2d0f8ad12eff
Yaroslav Lapin <ylapin@legalzoom.com>
2024-07-06 18:46:37 -0600

    update with client solid/react

--- src/components/solid/InnerCounter.tsx
+++ src/components/solid/InnerCounter.tsx
@@ -1,7 +1,7 @@
-/** @jsxImportSource react */
+/** @jsxImportSource solid-js */
 
-import { useState } from "react";
-import styles from "./styles.module.css";
+import { createSignal } from "solid-js";
+import styles from "../react/styles.module.css";
 
 interface CounterProps {
   initialCount?: number;
@@ -9,7 +9,7 @@ interface CounterProps {
 
 export function InnerCounter({ initialCount = 99 }: CounterProps) {
   console.log("InnerCounter component rendered");
-  const [count, setCount] = useState(initialCount);
+  const [count, setCount] = createSignal(initialCount);
 
   const increment = () => {
     setCount((prevCount) => prevCount + 1);
@@ -21,11 +21,11 @@ export function InnerCounter({ initialCount = 99 }: CounterProps) {
 
   return (
     <>
-      <button data-action="dec" className={styles.btn} onClick={decrement}>
+      <button data-action="dec" class={styles.btn} onClick={decrement}>
         -
       </button>
-      <span className={styles.count}>{count}</span>
-      <button data-action="inc" className={styles.btn} onClick={increment}>
+      <span class={styles.count}>{count()}</span>
+      <button data-action="inc" class={styles.btn} onClick={increment}>
         +
       </button>
     </>

2015969dddcc3026f88c432cc289f48a3179d36c
JLarky <jlarky@gmail.com>
2024-07-10 17:33:35 -0600

    Svelte 5 https://twitter.com/JLarky/status/1809802469159141519

--- src/components/svelte/SvelteCounter.svelte
+++ src/components/svelte/SvelteCounter.svelte
@@ -1,23 +1,20 @@
-<!-- @component
-A counter written with Svelte
--->
-<script lang="ts">
-	let count = 0;
+<script>
+  let count = $state(0);
 
-	function add() {
-		count += 1;
-	}
+  function add() {
+    count += 1;
+  }
 
-	function subtract() {
-		count -= 1;
-	}
+  function subtract() {
+    count -= 1;
+  }
 </script>
 
 <div class="counter">
-	<button on:click={subtract}>-</button>
-	<pre>{count}</pre>
-	<button on:click={add}>+</button>
+  <button on:click={subtract}>-</button>
+  <pre>{count}</pre>
+  <button on:click={add}>+</button>
 </div>
 <div class="counter-message">
-	<slot />
+  <slot />
 </div>
